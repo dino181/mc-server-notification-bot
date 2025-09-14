e3d15e8fe81cab0823091f609181bc84a0434729
Matthijs <mattkorevaar@hotmail.com>
2025-09-14 21:39:40 +0200

    Add multiple tries for pinging and increase interval

--- minecraft.py
+++ minecraft.py
@@ -1,5 +1,8 @@
 from enum import Enum, auto
 import mcstatus
+import time
+
+TRIES = 5
 
 
 class Status(Enum):
@@ -8,10 +11,13 @@ class Status(Enum):
 
 
 def get_status(server_adress: str) -> Status:
-    try:
-        server = mcstatus.JavaServer.lookup(server_adress)
-        server.ping()
-        return Status.ON
+    for i in range(TRIES):
+        try:
+            server = mcstatus.JavaServer.lookup(server_adress)
+            server.ping()
+            return Status.ON
 
-    except Exception:
-        return Status.OFF
+        except Exception:
+            print(f"Failed to ping server: {i + 1}/{TRIES}")
+
+    return Status.OFF
